42f477720f5914f39240bd5fa1c5d465a7bc9154
nao7016 <naohiro.nisi@gmail.com>
2025-08-06 16:34:35 +0900

    added configuration to enable/disable recipes / added QoL recipes

--- src/main/java/com/nao7016/ClayiumAdditions/common/Config.java
+++ src/main/java/com/nao7016/ClayiumAdditions/common/Config.java
@@ -7,11 +7,39 @@ import net.minecraftforge.common.config.Configuration;
 public class Config {
 
     public static String greeting = "Hello World";
+    public static boolean cfgSplittedEnergeticClayEnabled;
+    public static boolean cfgAutoWaterWheelEnabled;
+    public static boolean cfgFishesTransform;
+    public static boolean cfgCalciumChlorideTransform;
+    public static boolean cfgModeSky;
 
     public static void synchronizeConfiguration(File configFile) {
         Configuration configuration = new Configuration(configFile);
 
         greeting = configuration.getString("greeting", Configuration.CATEGORY_GENERAL, greeting, "How shall I greet?");
+
+        cfgSplittedEnergeticClayEnabled = configuration.getBoolean(
+            "EnableSplittedEnergeticClay",
+            "item",
+            true,
+            "If true, Splitted Energetic Clay will be enabled.");
+        cfgAutoWaterWheelEnabled = configuration
+            .getBoolean("EnableAutoWaterWheel", "item", true, "If true, Auto Water Wheel will be enabled.");
+        cfgFishesTransform = configuration.getBoolean(
+            "EnableTransformerFishRecipe",
+            "recipe",
+            true,
+            "If true, you can craft fish with Transformer by chicken.");
+        cfgCalciumChlorideTransform = configuration.getBoolean(
+            "EnableTransformerCalciumChlorideRecipe",
+            "recipe",
+            true,
+            "If true, Calcium Chloride can be transformed into Impure Calcium.");
+        cfgModeSky = configuration.getBoolean(
+            "SkyMode",
+            "mode",
+            false,
+            "If true, added recipes for skyblock. This mode is mainly for modpacks.");
 
         if (configuration.hasChanged()) {
             configuration.save();
--- src/main/java/com/nao7016/ClayiumAdditions/recipe/CARecipes.java
+++ src/main/java/com/nao7016/ClayiumAdditions/recipe/CARecipes.java
@@ -7,7 +7,9 @@ import net.minecraft.item.ItemStack;
 
 import com.nao7016.ClayiumAdditions.common.CABlocks;
 import com.nao7016.ClayiumAdditions.common.CAItems;
+import com.nao7016.ClayiumAdditions.common.Config;
 
+import cpw.mods.fml.common.registry.GameRegistry;
 import mods.clayium.block.CBlocks;
 import mods.clayium.core.ClayiumCore;
 import mods.clayium.item.CMaterials;
@@ -19,60 +21,82 @@ public class CARecipes {
         registerCESpritted();
         registerTransformer();
         registerAssembler();
+        registerCrafting();
+    }
+
+    private static void registerCrafting() {
+        GameRegistry.addShapelessRecipe(
+            CMaterials.get(CMaterials.CLAY, CMaterials.BALL, 8),
+            CMaterials.get(CMaterials.CLAY, CMaterials.LARGE_BALL));
+        GameRegistry.addShapelessRecipe(
+            CMaterials.get(CMaterials.CLAY, CMaterials.BALL, 4),
+            CMaterials.get(CMaterials.CLAY, CMaterials.BLOCK));
     }
 
     private static void registerCESpritted() {
-        for (int tier = 4; tier <= 12; tier++) {
-            CRecipes.recipeCuttingMachine.addRecipe(
-                new ItemStack(CBlocks.blockCompressedClay, 1, tier),
-                4,
-                new ItemStack(CAItems.clayEnergy, 9, tier),
-                10L,
-                (long) ClayiumCore.divideByProgressionRateI(10));
+        if (Config.cfgSplittedEnergeticClayEnabled) {
+            for (int tier = 4; tier <= 12; tier++) {
+                CRecipes.recipeCuttingMachine.addRecipe(
+                    new ItemStack(CBlocks.blockCompressedClay, 1, tier),
+                    4,
+                    new ItemStack(CAItems.clayEnergy, 9, tier),
+                    10L,
+                    ClayiumCore.divideByProgressionRateI(10));
+            }
         }
     }
 
     private static void registerTransformer() {
-        CRecipes.registerStackChainRecipes(
-            CRecipes.recipeTransformer,
-            ii(
-                i(Items.chicken, 2, 0),
-                i(Items.fish, 1, 0),
-                i(Items.fish, 1, 1),
-                i(Items.fish, 1, 2),
-                i(Items.fish, 1, 3)),
-            new int[] { 2, 1, 1, 1, 1 },
-            new int[] { 0, 9, 9, 9, 9 },
-            200);
-        CRecipes.recipeTransformer.addRecipe(
-            CMaterials.get(CMaterials.CALCIUM_CHLORIDE, CMaterials.DUST),
-            7,
-            CMaterials.get(CMaterials.IMPURE_CALCIUM, CMaterials.DUST),
-            200000,
-            200);
+        if (Config.cfgFishesTransform) {
+            CRecipes.registerStackChainRecipes(
+                CRecipes.recipeTransformer,
+                ii(
+                    i(Items.chicken, 2, 0),
+                    i(Items.fish, 1, 0),
+                    i(Items.fish, 1, 1),
+                    i(Items.fish, 1, 2),
+                    i(Items.fish, 1, 3)),
+                new int[] { 2, 1, 1, 1, 1 },
+                new int[] { 0, 9, 9, 9, 9 },
+                200);
+        }
+        if (Config.cfgCalciumChlorideTransform) {
+            CRecipes.recipeTransformer.addRecipe(
+                CMaterials.get(CMaterials.CALCIUM_CHLORIDE, CMaterials.DUST),
+                7,
+                CMaterials.get(CMaterials.IMPURE_CALCIUM, CMaterials.DUST),
+                200000,
+                200);
+        }
     }
 
     private static void registerAssembler() {
-        CRecipes.recipeAssembler.addRecipe(
-            ii(i(CBlocks.blockDenseClayWaterWheel), CMaterials.get(CMaterials.IND_CLAY, CMaterials.LARGE_PLATE)),
-            0,
-            3,
-            ii(i(CABlocks.blockAutoSimpleWaterWheel)),
-            40L,
-            120L);
-        CRecipes.recipeAssembler.addRecipe(
-            ii(i(CABlocks.blockAutoSimpleWaterWheel), CMaterials.get(CMaterials.ADVIND_CLAY, CMaterials.LARGE_PLATE)),
-            0,
-            4,
-            ii(i(CABlocks.blockAutoBasicWaterWheel)),
-            200L,
-            120L);
-        CRecipes.recipeAssembler.addRecipe(
-            ii(i(CABlocks.blockAutoBasicWaterWheel), CMaterials.get(CMaterials.IMPURE_SILICON, CMaterials.LARGE_PLATE)),
-            0,
-            4,
-            ii(i(CABlocks.blockAutoAdvancedWaterWheel)),
-            1000L,
-            120L);
+        if (Config.cfgAutoWaterWheelEnabled) {
+            CRecipes.recipeAssembler.addRecipe(
+                ii(i(CBlocks.blockDenseClayWaterWheel), CMaterials.get(CMaterials.IND_CLAY, CMaterials.LARGE_PLATE)),
+                0,
+                3,
+                ii(i(CABlocks.blockAutoSimpleWaterWheel)),
+                40L,
+                120L);
+            CRecipes.recipeAssembler.addRecipe(
+                ii(
+                    i(CABlocks.blockAutoSimpleWaterWheel),
+                    CMaterials.get(CMaterials.ADVIND_CLAY, CMaterials.LARGE_PLATE)),
+                0,
+                4,
+                ii(i(CABlocks.blockAutoBasicWaterWheel)),
+                200L,
+                120L);
+            CRecipes.recipeAssembler.addRecipe(
+                ii(
+                    i(CABlocks.blockAutoBasicWaterWheel),
+                    CMaterials.get(CMaterials.IMPURE_SILICON, CMaterials.LARGE_PLATE)),
+                0,
+                4,
+                ii(i(CABlocks.blockAutoAdvancedWaterWheel)),
+                1000L,
+                120L);
+        }
     }
 }
