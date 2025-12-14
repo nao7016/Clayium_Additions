fef969818e2ef53f157c1f2f4e77526775f3a388
nao7016 <naohiro.nisi@gmail.com>
2025-12-14 14:21:31 +0900

    spotless

--- src/main/java/com/nao7016/ClayiumAdditions/compat/craftguide/MachineRecipes.java
+++ src/main/java/com/nao7016/ClayiumAdditions/compat/craftguide/MachineRecipes.java
@@ -3,7 +3,11 @@ package com.nao7016.ClayiumAdditions.compat.craftguide;
 import net.minecraft.item.ItemStack;
 
 import mods.clayium.block.CBlocks;
-import uristqwerty.CraftGuide.api.*;
+import uristqwerty.CraftGuide.api.ConstructedRecipeTemplate;
+import uristqwerty.CraftGuide.api.CraftGuideAPIObject;
+import uristqwerty.CraftGuide.api.RecipeGenerator;
+import uristqwerty.CraftGuide.api.RecipeProvider;
+import uristqwerty.CraftGuide.api.RecipeTemplateBuilder;
 
 public class MachineRecipes extends CraftGuideAPIObject implements RecipeProvider {
 
